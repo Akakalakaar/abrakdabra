5746974f2788882ce7e3d03357bf9da374b8ee1c
SHIVANSH SONI <88659482+Akakalakaar@users.noreply.github.com>
2022-07-04 16:45:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 3000);
+}
+
+function bass() {
+  window.open("./sections/bass.html");
+}
+function adv() {
+  window.open("./sections/adv.html");
+}
+
+function opennav() {
+  if (document.getElementById("navul").style.display == "block") {
+    document.getElementById("navul").style.display = "none";
+  } else {
+    document.getElementById("navul").style.display = "block";
+  }
+}
